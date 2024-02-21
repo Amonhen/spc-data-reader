eb9f12540b9970bba728751381b36c6f6903894e
Petar Zelyamov <elessar4531@me.com>
2024-02-21 17:11:57 +0200

    SQLite DB update

--- electron/main.ts
+++ electron/main.ts
@@ -1,17 +1,16 @@
 import path from 'path'
 import { app, BrowserWindow } from 'electron'
-import {pathToFileURL} from "node:url";
+
+// The built directory structure
 //
-// // The built directory structure
-// //
-// // ├─┬ dist-electron
-// // │ ├─┬ main
-// // │ │ └── index.js
-// // │ ├─┬ preload
-// // │ │ └── index.js
-// // │ ├─┬ renderer
-// // │ │ └── index.html
-//
+// ├─┬ dist-electron
+// │ ├─┬ main
+// │ │ └── index.js
+// │ ├─┬ preload
+// │ │ └── index.js
+// │ ├─┬ renderer
+// │ │ └── index.html
+
 process.env.ROOT = path.join(__dirname, '..')
 process.env.DIST = path.join(process.env.ROOT, 'dist-electron')
 process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL
@@ -19,104 +18,28 @@ process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL
     : path.join(process.env.ROOT, '.output/public')
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 
-// let win: BrowserWindow
+let win: BrowserWindow
 const preload = path.join(process.env.DIST, 'preload.js')
-//
-// const modulePath = path.join(process.env.ROOT, '.output/server/index.mjs');
-// const moduleUrl = pathToFileURL(modulePath).href;
-//
-// // let startServer: Function|undefined = undefined
-//
-// // import(moduleUrl).then((data)=> {
-// //    startServer = data.default
-// // });
-//
-// async function bootstrap() {
-//    win = new BrowserWindow({
-//       title: "SPC Data",
-//       webPreferences: {
-//          preload,
-//          nodeIntegrationInWorker: true,
-//          contextIsolation: false,
-//          nodeIntegration: true,
-//          webSecurity: false,
-//       },
-//    })
-//
-//    if (process.env.VITE_DEV_SERVER_URL) {
-//      await win.loadURL(process.env.VITE_DEV_SERVER_URL)
-//      win.webContents.openDevTools()
-//    } else {
-//      await win.loadFile(path.join(process.env.VITE_PUBLIC!, 'index.html'))
-//    }
-// }
-//
-// app.whenReady()
-//     .then(()=> {
-//        import(moduleUrl).then((data)=> {
-//           data.default()
-//           bootstrap().then()
-//        });
-//     })
 
-// const { app, BrowserWindow } = require('electron')
-// const path =  require('path')
-// const { pathToFileURL } = require('url');
+async function bootstrap() {
+   win = new BrowserWindow({
+      title: "SPC Data",
+      webPreferences: {
+         preload,
+         nodeIntegrationInWorker: true,
+         contextIsolation: false,
+         nodeIntegration: true,
+         webSecurity: false,
+      },
+   })
 
-// /**
-//  * PreConfigure
-//  */
-//
-// // @ts-ignore
-// process.env.ELECTRON_DEV == 1 ?
-//     process.env.ROOT = path.resolve(__dirname, "../../") : //dev root folder
-//     process.env.ROOT = path.join(process.resourcesPath) //production root folder
-
-
-/**
- * Create function to run website in SSR
- */
-const startWebServer = async () => {
-    // @ts-ignore
-    const modulePath = path.join(process.env.ROOT, '.output/server/index.mjs');
-    const moduleUrl = pathToFileURL(modulePath).href;
-    const { default: startServer } = await import(moduleUrl);
-
-    if (typeof startServer === 'function') {
-        startServer();
-    }
+   if (process.env.VITE_DEV_SERVER_URL) {
+     await win.loadURL(process.env.VITE_DEV_SERVER_URL)
+     win.webContents.openDevTools()
+   } else {
+     await win.loadFile(path.join(process.env.VITE_PUBLIC!, 'index.html'))
+   }
 }
 
-
-/**
- * Create function to create window
- */
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload,
-            nodeIntegrationInWorker: true,
-            contextIsolation: false,
-            nodeIntegration: true,
-            webSecurity: false,
-        }
-    })
-
-    win.loadURL('http://localhost:3000')
-}
-
-/**
- * Create function when app is ready
- */
 app.whenReady()
-    .then(startWebServer)
-    .then(createWindow)
-
-/**
- * Close the app when all windows are closed
- */
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+    .then(bootstrap)
